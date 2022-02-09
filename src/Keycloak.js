edbae0c4c598d7421ad62c966ca293eb0e25021d
Marek Czernek <mczernek@redhat.com>
2022-02-09 15:04:43 +0100

    Correct secret

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
  url: "https://keycloak-sso.apps.ocp4.example.com/auth",
- realm: "ad240-realm",
+ realm: "ad240",
  clientId: "ad240-secret",
 });
 
